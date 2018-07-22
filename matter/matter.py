cc8278cc452a1021f641e9d8d0e45f1dcfa6a89e
孙亚东 <xxxspy@126.com>
2018-07-22 16:06:26 +0800

    add namespaces

--- matter/matter.py
+++ matter/matter.py
@@ -3,6 +3,15 @@ from . import settings
 from pscript import py2js
 import webbrowser
 
+_NAMESPACES = {}
+
+def add_namespace(name, script):
+    '''
+    name: 'jquery' for example
+    script: '<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>' for example
+    '''
+    _NAMESPACES[name] = script
+
 def show(objs):
     if isinstance(objs, (list, tuple)):
         scripts = [py2js(o) for o in objs]
@@ -12,8 +21,10 @@ def show(objs):
         last_call = objs.__name__ + '()'
     scripts.append(last_call)
     scripts = '\n'.join(scripts)
+    namespaces = list(_NAMESPACES.keys())
+    namespaces = '\n'.join(namespaces) if namespaces else ''
     template = settings.TEMPLATE.read_text(encoding='utf8')
-    html = template.format(script=scripts)
+    html = template.format(script=scripts, namespaces=namespaces)
     with NamedTemporaryFile(suffix='.html', 
                             prefix='matter.py.',
                             delete=False, 
@@ -35,6 +46,8 @@ def register_notebook():
     dir_path = Path(os.path.join(cwd, dir_name))
     dir_path.mkdir(parents=True, exist_ok=True)
     template = settings.TEMPLATE.read_text(encoding='utf8')
+    namespaces = list(_NAMESPACES.keys())
+    namespaces = '\n'.join(namespaces) if namespaces else ''
     @register_cell_magic
     def matter_inline(line, cell):
         kws = line.split(' ')
@@ -47,7 +60,7 @@ def register_notebook():
         filename = f"{dir_name}/{notebook}-{file_id}.html"
         js = py2js(cell)
         with open(filename, "w", encoding='utf8') as fp:
-            fp.write(template.format(script=js, name=filename, page_link=filename) )
+            fp.write(template.format(script=js, name=filename, page_link=filename, namespaces=namespaces) )
         if 'height' not in kwargs:
             kwargs['height'] = '200px'
         return IFrame(filename, width="100%", **kwargs)
